26938282170d3ecc043b1d997ec758e52101df7d
Gianbattista Vivolo <gianbattista.vivolo@gmail.com>
2024-05-23 11:08:46 +0200

    Initial Commit

---
+++ src/components/patterns.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React, { useState } from "react";
+
+type Props = {
+  patterns: {
+    attributes: { titolo: string; descrizione: string };
+  }[];
+};
+
+const Patterns = ({ patterns }: Props) => {
+  // const [patterns, setPatterns] = useState<
+  //
+  // >([]);
+  return <div>Patterns</div>;
+};
+
+export default Patterns;
